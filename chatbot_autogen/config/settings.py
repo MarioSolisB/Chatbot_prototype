7368751655b22b5865e1864687a267d90e079f11
Maki1209 <m.f.solis.benites@gmail.com>
2024-12-26 14:48:39 -0300

    create first folders and files in autogen chatbot

---
+++ chatbot_autogen/config/settings.py
@@ -0,0 +1,27 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv(".env")
+
+# --- OpenAI ---
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+# --- Anthropic ---
+ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+
+# --- Telegram ---
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+
+# --- GPT model ----
+gpt_model = "gpt-4o-mini" #"gpt-4o"
+
+# --- Agent Names ---
+REAL_ESTATE_AGENT_NAME = "NEXUS_Agent"
+USER_PROXY_NAME = "Client"
+
+# --- Interface Settings ---
+CLEAR_COMMAND = "clear"
+EXIT_COMMAND = "exit"
+
+SLEEP_TIME_BETWEEN_RETRIEVALS = 1
