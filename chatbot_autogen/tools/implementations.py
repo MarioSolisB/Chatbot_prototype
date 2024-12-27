f68d85dcd4ed0f282dca5be81eb6cfce2be6a105
Maki1209 <m.f.solis.benites@gmail.com>
2024-12-26 23:30:49 -0300

    delete folder

--- chatbot_autogen/tools/implementations.py
+++
@@ -1,5 +0,0 @@
-def send_brochure(unit_number: str, email: str = None) -> str:
-    return f"[IN CRM: Sending brochure for unit {unit_number} to {email if email else 'client'}]"
-
-def schedule_visit(date: str, time: str, client_name: str, email: str = None) -> str:
-    return f"[IN CRM: Scheduling visit for {client_name} on {date} at {time}]"
